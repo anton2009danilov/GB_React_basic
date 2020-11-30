022e4879af35e07441293780604926447c3f89da
anton2009danilov <anton2009danilov@yandex.ru>
2020-11-30 03:35:38 +0300

    Выполнил задание 6: реализовать возможность отправки нового сообщения с фиксированным текстом:

--- static_src/index.jsx
+++ static_src/index.jsx
@@ -3,12 +3,25 @@ import ReactDom from 'react-dom';
 
 let messages = ["Привет", "Как дела?"];
 
-const MessageComponent = (props) => <div>{ props.text }</div>
+const MessageComponent = (props) => <div>{ props.text }</div>;
 const MessageField = (props) => {
     return props.messages.map(message => <MessageComponent text={ message }/>);
+};
+const addMessage = function() {
+    // console.log(123);
+    messages.push("Всё ОК");
+
+    renderApp()
 }
 
-ReactDom.render(
-    <MessageField messages= {messages}></MessageField>,
-    document.getElementById('root')
-);
+function renderApp() {
+    ReactDom.render(
+        <div>
+            <button id='newMessageBtn' onClick={addMessage}>Add message</button>
+            <MessageField messages= {messages}></MessageField>
+        </div>,
+        document.getElementById('root')
+    );
+}
+
+renderApp();
