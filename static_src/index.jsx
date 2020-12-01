c9aecbbcfab1ef4f2d42b2b9ae1a4dd40ff6baa6
anton2009danilov <anton2009danilov@yandex.ru>
2020-12-02 00:48:30 +0300

    удалил не используемый код

--- static_src/index.jsx
+++ static_src/index.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-const MessageComponent = (props) => <div>{ props.text }</div>;
-
 ReactDOM.render(
     <App/>,
     document.getElementById('root')
