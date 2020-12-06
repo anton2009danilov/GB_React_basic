6f6b2ab179da06466453293efa07f4f9c1ae875c
anton2009danilov <anton2009danilov@yandex.ru>
2020-12-06 08:55:38 +0300

    Создал компонент ChatList, подключил @material-ui/icons, @material-ui/core

---
+++ static_src/components/ChatList.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { List, ListItem } from "material-ui";
+import ChatIcon from "@material-ui/icons/Chat";
+
+// import Chat from "@material-ui/icons/Chat";
+
+export default function ChatList() {
+  return (
+    <div className="col-sm-4 p-0">
+      <List
+        style={{ backgroundColor: "lavender" }}
+        className="d-flex flex-sm-row flex-wrap flex-lg-column mb-2"
+      >
+        <ListItem
+          primaryText="Chat 1"
+          leftIcon={<ChatIcon color="primary" />}
+        />
+        <ListItem
+          primaryText="Chat 2"
+          leftIcon={<ChatIcon color="primary" />}
+        />
+        <ListItem
+          primaryText="Chat 3"
+          leftIcon={<ChatIcon color="primary" />}
+        />
+      </List>
+    </div>
+  );
+}
--- static_src/components/Layout.jsx
+++ static_src/components/Layout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import MessageField from "./MessageField";
 import Header from "./Header";
+import MessageField from "./MessageField";
+import ChatList from "./ChatList";
 
 export default function Layout() {
   return (
@@ -8,6 +9,7 @@ export default function Layout() {
       <Header />
       <div className="container">
         <div className="row">
+          <ChatList />
           <MessageField />
         </div>
       </div>
