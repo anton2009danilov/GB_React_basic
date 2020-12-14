925d7b691079298aa0b9e51a7de1d560782a4f00
anton2009danilov <anton2009danilov@yandex.ru>
2020-12-14 20:12:06 +0300

    Создал и подключил chatReducer

---
+++ static_src/reducers/chatReducer.js
@@ -0,0 +1,43 @@
+import update from 'react-addons-update';
+import { SEND_MESSAGE } from '../actions/messageActions';
+import { ADD_CHAT } from '../actions/chatActions';
+
+const initialStore = {
+	chats: {
+		1: { id: 1, title: 'Чат 1', messageList: [1] },
+		2: { id: 2, title: 'Чат 2', messageList: [2] },
+		3: { id: 3, title: 'Чат 3', messageList: [] },
+	},
+};
+
+export default function chatReducer(store = initialStore, action) {
+	switch (action.type) {
+		case SEND_MESSAGE: {
+			return update(store, {
+				chats: {
+					$merge: {
+						[action.chatId]: {
+							title: store.chats[action.chatId].title,
+							messageList: [...store.chats[action.chatId].messageList, action.messageId]
+						}
+					}
+				}
+			});
+		}
+		case ADD_CHAT: {
+			const chatId = Object.keys(store.chats).length + 1;
+			return update(store, {
+				chats: {
+					$merge: {
+						[chatId]: {
+							title: action.title,
+							messageList: []
+						}
+					}
+				}
+			});
+		}
+		default:
+			return store;
+	}
+}
